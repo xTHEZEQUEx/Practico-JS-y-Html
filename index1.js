842836af88fac449434d58c0e682f9f79d564a42
xTHEZEQUEx <nicolassequera8@gmail.com>
2023-08-25 14:40:54 -0500

    Final class

--- index1.js
+++ index1.js
@@ -5,23 +5,30 @@ const menuMobile = document.querySelector('.mobile-menu');
 const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
 const asideOrder = document.querySelector('.product-detail')
 const cardsContainer = document.querySelector('.cards-container')
+const productDetails = document.querySelector('.details-product')
+const imgProductDetailClose = document.querySelector('.product-detail-close')
 
 navEmail.addEventListener('click',toggleMenu)
 hamburguerMenu.addEventListener('click',toggleMobile)
 menuShoppingCart.addEventListener('click', toggleShoppingCart)
+imgProductDetailClose.addEventListener('click',closeProductDetails)
 function toggleMenu() {
     const isAsideClosed = asideOrder.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
     if(!isAsideClosed){
         asideOrder.classList.add('inactive')
+    } else if (isDesktopMenuClosed){
+        productDetails.classList.add('inactive')        
     }
     desktopMenu.classList.toggle('inactive')
 }   
 function toggleMobile() {
     const isAsideClosed = asideOrder.classList.contains('inactive')
+    
     if(!isAsideClosed){
         asideOrder.classList.add('inactive')
-    }
-
+    } 
+    closeProductDetails()
     menuMobile.classList.toggle('inactive') 
 }
 function toggleShoppingCart() {
@@ -34,7 +41,19 @@ function toggleShoppingCart() {
         desktopMenu.classList.add('inactive')
     }
     asideOrder.classList.toggle('inactive') 
-    
+
+    const isDetailsAsideClosed = productDetails.classList.contains('inactive')
+    if (!isDetailsAsideClosed){
+        productDetails.classList.add('inactive')
+    }
+}
+
+ function showProductDetails(){
+    asideOrder.classList.add('inactive')
+    productDetails.classList.remove('inactive')
+ }
+function closeProductDetails() {
+    productDetails.classList.add('inactive')
 }
 const productList =[]
 productList.push({
@@ -78,6 +97,24 @@ function addProducts(arrayProductos) {
         productCard.appendChild(productImg)
         productCard.appendChild(productInfo)
         cardsContainer.appendChild(productCard)
+
+        productImg.addEventListener('click', showProductDetails)
     }
 }
 addProducts(productList) 
+
+/*<aside class="details-product inactive">
+    <div class="product-detail-close">
+      <img src="./icons/icon_close.png" alt="close">
+    </div>
+    <img src="https://i.pinimg.com/564x/90/e2/61/90e261b6764a0a7c003bd61ec843a62c.jpg" alt="bike">
+    <div class="info-product">
+      <p>$35,00</p>
+      <p>Bike</p>
+      <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+      <button class="primary-button add-to-cart-button">
+        <img src="./icons/bt_add_to_cart.svg" alt="add to cart">
+        Add to cart
+      </button>
+    </div>
+  </aside>*/
