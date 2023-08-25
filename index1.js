522dd739db846601bb4dd65f044340c4ccd68192
xTHEZEQUEx <nicolassequera8@gmail.com>
2023-08-25 10:09:24 -0500

    Adding hamburguer menu and some media queries

--- index1.js
+++ index1.js
@@ -1,7 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerMenu = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu');
 navEmail.addEventListener('click',toggleMenu)
+hamburguerMenu.addEventListener('click',toggleMobile)
 
 function toggleMenu() {
     desktopMenu.classList.toggle('inactive')
 }   
+function toggleMobile() {
+    console.log('click')
+    menuMobile.classList.toggle('inactive') 
+}
