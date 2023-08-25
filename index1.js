f0f68d4615d8fd39f0f2f19ee2702948ffdb6bf0
xTHEZEQUEx <nicolassequera8@gmail.com>
2023-08-24 19:11:49 -0500

    Menu toggle

---
+++ index1.js
@@ -0,0 +1,7 @@
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+navEmail.addEventListener('click',toggleMenu)
+
+function toggleMenu() {
+    desktopMenu.classList.toggle('inactive')
+}   
