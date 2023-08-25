093311f96a595df495cf06646f5a523f2e39bfea
xTHEZEQUEx <nicolassequera8@gmail.com>
2023-08-25 10:54:12 -0500

    Adding aside order and some media queries

--- index1.js
+++ index1.js
@@ -2,13 +2,36 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburguerMenu = document.querySelector('.menu')
 const menuMobile = document.querySelector('.mobile-menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const asideOrder = document.querySelector('.product-detail')
+
 navEmail.addEventListener('click',toggleMenu)
 hamburguerMenu.addEventListener('click',toggleMobile)
-
+menuShoppingCart.addEventListener('click', toggleShoppingCart)
 function toggleMenu() {
+    const isAsideClosed = asideOrder.classList.contains('inactive')
+    if(!isAsideClosed){
+        asideOrder.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive')
 }   
 function toggleMobile() {
-    console.log('click')
+    const isAsideClosed = asideOrder.classList.contains('inactive')
+    if(!isAsideClosed){
+        asideOrder.classList.add('inactive')
+    }
+
     menuMobile.classList.toggle('inactive') 
+}
+function toggleShoppingCart() {
+    const isMenuMobileClosed = menuMobile.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    
+    if (!isMenuMobileClosed) {
+        menuMobile.classList.add('inactive')
+    } else if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive')
+    }
+    asideOrder.classList.toggle('inactive') 
+    
 }
